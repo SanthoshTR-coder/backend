e54ef2c3385d1d6494949ab53c64383cfbf1946a
SanthoshTR-Coder <santhoshtr0912@gmail.com>
2025-06-19 09:42:35 +0530

    final

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,10 @@
-export const PORT = 5555;
+// export const PORT = 5555;
 
-export const mongoDBURL =
-  'mongodb+srv://varunquanth0001:VhE01HkT89q5u8oC@cluster0.t4jif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// export const mongoDBURL =
+//   'mongodb+srv://varunquanth0001:VhE01HkT89q5u8oC@cluster0.t4jif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const PORT = process.env.PORT;
+export const mongoDBURL = process.env.MONGODB_URL;
